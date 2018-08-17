bcb2d67c840956b9ba7aa1a639db644a9bec6dc6
Brian Kang <brianhkang1@gmail.com>
2018-08-17 23:49:21 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,7 +1,7 @@
 def run_guessing_game
   num_random = "2"
   
-  "Guess a number between 1 and 6."
+  puts "Guess a number between 1 and 6."
   user_input = gets.chomp
   
   if user_input == "exit"
